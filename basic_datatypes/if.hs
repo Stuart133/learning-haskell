b077ab7f5bb839ce52caf32ba01d6df32e7e0b24
Stuart Reid <stuart1334@gmail.com>
2021-03-28 12:45:03 +0100

    More if

--- basic_datatypes/if.hs
+++ basic_datatypes/if.hs
@@ -6,3 +6,18 @@ w = "wut"
 ifFunc x = if (x + 1 == 1)
             then a
             else w
+
+greetIfCool coolness =
+  if cool
+    then putStrLn "eyyyyy. What's shakin'?"
+  else
+    putStrLn "pshhh."
+  where cool = coolness == "downright frosty yo"
+
+greetIfCool2 coolness =
+  if cool coolness
+    then putStrLn "eyyyyy. What's shakin'?"
+  else
+    putStrLn "pshhh."
+  where cool v =
+            v == "downright frosty yo"
