cc9785040ba6e4b0928845177f1bb4bc9dc2a7ef
Stuart Reid <stuart1334@gmail.com>
2021-03-28 13:56:45 +0100

    Couple more exercises

---
+++ basic_datatypes/pets.hs
@@ -0,0 +1,6 @@
+module Pets where
+
+-- Type alias
+type Name = String
+
+data Pet = Cat | Dog Name
---
+++ basic_datatypes/syntax.hs
@@ -0,0 +1,10 @@
+module Syntax where
+
+x = (+)
+
+f xs = x w 1
+    where w = length xs
+
+y x = x
+
+g x = fst x
