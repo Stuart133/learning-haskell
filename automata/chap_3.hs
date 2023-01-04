f16964a6833c62ea169cc8322f1f49c9190cb10f
Stuart133 <stuart1334@gmail.com>
2023-01-04 15:18:31 +0000

    More exercises

--- automata/chap_3.hs
+++ automata/chap_3.hs
@@ -3,3 +3,18 @@ module Chap3 where
 root :: Float -> Float -> Float -> Float
 root a b c = 
     ((-b) + sqrt (b ^ 2 - (4 * a * c))) / (2 * a)
+
+hour :: Int -> Int
+hour n = 
+    (mod (div n 60) 12) + 1
+
+between :: Int -> Int -> Int -> Int
+between a b c 
+    | (a >= b && a <= c) || (a <= b && a >= c) = a
+    | (b >= a && b <= c) || (b <= a && b >= c) = b
+    | otherwise = c
+
+xor :: Bool -> Bool -> Bool
+xor True False = True
+xor False True = True
+xor _ _ = False
