a60474c97aed85285cd13eb5891bef46f6b3ba9d
Stuart133 <stuart1334@gmail.com>
2023-02-01 11:04:17 +0000

    Expression trees

--- automata/chap_15.hs
+++ automata/chap_15.hs
@@ -1,5 +1,7 @@
 module Chap15 where
 
+import Control.Monad
+
 data Season = Winter | Spring | Summer | Fall deriving (Eq, Show)
 
 next :: Season -> Season
@@ -25,13 +27,34 @@ area (Rect w h) = w * h
 
 data Pair a b = Pair a b deriving (Eq, Show)
 
-data List a
-  = Nil
-  | Cons a (List a)
-  deriving (Eq, Show)
-
 data Maybe a = Nothing | Just a
   deriving (Eq, Show)
 
 data Either a b = Left a | Right b
   deriving (Eq, Show)
+
+data Fruit
+  = Apple String Bool
+  | Orange String Int
+  deriving (Eq, Show)
+
+isBloodOrange :: Fruit -> Bool
+isBloodOrange (Orange name _) = name == "Tarocco" || name == "Moro" || name == "Sanguinello"
+isBloodOrange _ = False
+
+bloodOrangeSegments :: [Fruit] -> Int
+bloodOrangeSegments fs = sum [n | Orange _ n <- filter isBloodOrange fs]
+
+worms :: [Fruit] -> Int
+worms fs = foldl (\i v -> if v then i + 1 else i) 0 [w | Apple _ w <- fs]
+
+data WideList a
+  = Nil
+  | Cons a (WideList a)
+  | Append (WideList a) (WideList a)
+  deriving (Eq, Show)
+
+lengthWide :: WideList a -> Int
+lengthWide Nil = 0
+lengthWide (Cons a l) = 1 + lengthWide l
+lengthWide (Append l r) = lengthWide l + lengthWide r
---
+++ automata/trees.hs
@@ -0,0 +1,115 @@
+module Trees where
+
+import Data.List (nub)
+
+data Exp
+  = Lit Int
+  | Exp `Add` Exp
+  | Exp `Mul` Exp
+  | If Cond Exp Exp
+  deriving (Eq)
+
+data Cond
+  = Eq Exp Exp
+  | Lt Exp Exp
+  | Gt Exp Exp
+  deriving (Eq)
+
+instance Show Exp where
+  show (Lit n) =
+    show n
+  show (Add e f) =
+    "(" ++ (show e ++ " + " ++ show f) ++ ")"
+  show (Mul e f) =
+    "(" ++ (show e ++ " * " ++ show f) ++ ")"
+  show (If c e f) = "if " ++ show c ++ " then " ++ show e ++ " else " ++ show f
+
+instance Show Cond where
+  show (Eq e f) = show e ++ " == " ++ show f
+  show (Lt e f) = show e ++ " < " ++ show f
+  show (Gt e f) = show e ++ " > " ++ show f
+
+evalExp :: Exp -> Int
+evalExp (Lit n) = n
+evalExp (Add e f) = evalExp e + evalExp f
+evalExp (Mul e f) = evalExp e * evalExp f
+evalExp (If c e f) = if evalCond c then evalExp e else evalExp f
+
+evalCond :: Cond -> Bool
+evalCond (Eq e f) = evalExp e == evalExp f
+evalCond (Lt e f) = evalExp e < evalExp f
+evalCond (Gt e f) = evalExp e > evalExp f
+
+e0 = Add (Lit 1) (Mul (Lit 2) (Lit 3))
+
+e1 = Mul (Add (Lit 1) (Lit 2)) (Lit 3)
+
+c0 = Lt e0 e1
+
+e2 = If c0 e0 e1
+
+type Name = String
+
+type Valn = Name -> Bool
+
+type Names = [Name]
+
+data Prop
+  = Var Name
+  | F
+  | T
+  | Not Prop
+  | Prop :||: Prop
+  | Prop :&&: Prop
+  deriving (Eq)
+
+instance Show Prop where
+  show (Var x) = x
+  show F = "F"
+  show T = "T"
+  show (Not p) = "(not " ++ show p ++ ")"
+  show (p :||: q) = "(" ++ show p ++ " || " ++ show q ++ ")"
+  show (p :&&: q) = "(" ++ show p ++ " && " ++ show q ++ ")"
+
+evalProp :: Valn -> Prop -> Bool
+evalProp vn (Var x) = vn x
+evalProp vn F = False
+evalProp vn T = True
+evalProp vn (Not p) = not (evalProp vn p)
+evalProp vn (p :||: q) = evalProp vn p || evalProp vn q
+evalProp vn (p :&&: q) = evalProp vn p && evalProp vn q
+
+valn :: Valn
+valn "a" = True
+valn "b" = True
+valn "c" = False
+valn "d" = True
+
+empty :: Valn
+empty y = error "undefined"
+
+extend :: Valn -> Name -> Bool -> Valn
+extend vn x b y
+  | x == y = b
+  | otherwise = vn y
+
+valns :: Names -> [Valn]
+valns [] = [empty]
+valns (x : xs) = [extend vn x b | vn <- valns xs, b <- [True, False]]
+
+names :: Prop -> Names
+names (Var x) = [x]
+names F = []
+names T = []
+names (Not p) = names p
+names (p :||: q) = nub (names p ++ names q)
+names (p :&&: q) = nub (names p ++ names q)
+
+satisfiable :: Prop -> Bool
+satisfiable p = or [evalProp vn p | vn <- valns (names p)]
+
+p0 = Var "a" :&&: Not (Var "a")
+
+p1 = (Var "a" :&&: Var "b") :||: (Not (Var "a") :&&: Not (Var "b"))
+
+p2 = (Var "a" :&&: Not (Var "b") :&&: (Var "c" :||: (Var "d" :&&: Var "b")) :||: (Not (Var "b") :&&: Not (Var "a"))) :&&: Var "c"
